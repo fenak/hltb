dadc210e66e0abf4d9c54cd0cdd2befafbaf89a6
Felipe Nakandakari <felipe@nakand.com>
2014-03-18 18:51:30 -0300

    minimum functionality

--- hltb.go
+++ hltb.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/moovweb/gokogiri"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type gameInfo struct {
+	label string
+	value string
+}
 
 func main() {
-	fmt.Printf("hltb!")
+	if len(os.Args) < 2 {
+		fmt.Println("Should pass on the game name.")
+		return
+	}
+
+	fmt.Println("hltb!")
+	fmt.Println("======================")
+
+	games := Scrap(strings.Join(os.Args[1:], " "))
+	for _, game := range games {
+		for _, info := range game {
+			fmt.Println(info.label + ": " + info.value)
+		}
+		fmt.Println("======================")
+	}
+}
+
+func Scrap(queryString string) [][]gameInfo {
+	post_url := "http://www.howlongtobeat.com/search_main.php?t=games&page=1&sorthead=&sortd=Normal&plat=&detail=0"
+	resp, _ := http.PostForm(post_url, url.Values{"queryString": {queryString}})
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	doc, _ := gokogiri.ParseHtml(body)
+	defer doc.Free()
+
+	rootNode := doc.Root() //.Search(xpath.Compile("//div[@id=\"suggestionsList_main\"]/li"))
+	gameNameNodes, _ := rootNode.Search("//h3/a")
+	mainStoryNodes, _ := rootNode.Search("//div[text()=\"Main Story\"]")
+	mainPlusExtraNodes, _ := rootNode.Search("//div[text()=\"Main + Extra\"]")
+	completionistNodes, _ := rootNode.Search("//div[text()=\"Completionist\"]")
+	combinedNodes, _ := rootNode.Search("//div[text()=\"Combined\"]")
+
+	games := make([][]gameInfo, len(gameNameNodes))
+	for i := 0; i < len(gameNameNodes); i++ {
+		games[i] = []gameInfo{
+			gameInfo{"Game Name", gameNameNodes[i].Content()},
+			gameInfo{"Main Story", mainStoryNodes[i].NextSibling().Content()},
+			gameInfo{"Main + Extra", mainPlusExtraNodes[i].NextSibling().Content()},
+			gameInfo{"Completionist", completionistNodes[i].NextSibling().Content()},
+			gameInfo{"Combined", combinedNodes[i].NextSibling().Content()},
+		}
+	}
+	return games
 }
